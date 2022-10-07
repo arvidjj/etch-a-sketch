e3339d8b36ec81bed1e92d2516bde60925591306
arvidjj <andy.oj14@gmail.com>
2022-10-07 20:26:19 -0300

    Added shadow option and fixed bugs

--- js/app.js
+++ js/app.js
@@ -39,27 +39,76 @@ window.addEventListener('mouseup', () => {
 })
 /*          */
 
+let shadowModeActive = false;
 let randomColorActive = false;
+let modesList = [shadowModeActive, randomColorActive];
 let currentColor = 'black';
+let lastColor = currentColor;
 
 let buttonRandomRGB = document.querySelector('#buttonRandomRGB');
 buttonRandomRGB.addEventListener('click', toggleRandom);
 
 function toggleRandom() {
-    randomColorActive = !randomColorActive;
-    if (randomColorActive) {
+    if (modesList[0]) {
+        unToggleButtons();
+        return;
+    }
+    unToggleButtons();
+    modesList[0] = !modesList[0];
+    
+    if (modesList[0]) {
         buttonRandomRGB.classList.add('active');
+        lastColor = currentColor;
     } else {
         buttonRandomRGB.classList.remove('active');
     }
 }
 
+
+let buttonShadowMode = document.querySelector('#buttonShadowMode');
+buttonShadowMode.addEventListener('click', toggleShadow);
+
+function toggleShadow() {
+    if (modesList[1]) {
+        unToggleButtons();
+        return;
+    }
+    unToggleButtons();
+    modesList[1] = !modesList[1];
+
+    if (modesList[1]) {
+        buttonShadowMode.classList.add('active');
+    } else {
+        buttonShadowMode.classList.remove('active');
+    }
+}
+
+let drawingOptionButtons = document.querySelectorAll('.drawingOption');
+
+function unToggleButtons() {
+    drawingOptionButtons.forEach(a => a.classList.remove('active'));
+    for (let i = 0; i < modesList.length; ++i) {
+        modesList[i] = false;
+    }
+    currentColor = lastColor;
+}
+
 function clickedGrid(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
     if (mouseDown) {
-        if (randomColorActive === true) {
+        if (modesList[0] === true) { //RANDOM RGB
             let randomColor = Math.floor(Math.random() * 16777215).toString(16);
             currentColor = "#" + randomColor;
+        } else if (modesList[1] === true) { // SHADOW MODE
+            if (this.style.backgroundColor === 'rgb(0, 0, 0)') {
+                return;
+            }
+            let gridColor = +this.style.backgroundColor.slice(14).slice(0, -1) + 0.1;
+            console.log(gridColor);
+            console.log(this.style.backgroundColor);
+
+            let shadowColor = `rgba(0, 0, 0, ${gridColor})`;
+            currentColor = shadowColor;
         }
         this.style.backgroundColor = currentColor;
     }
@@ -72,4 +121,6 @@ function resetGrids() {
 }
 
 let buttonClear = document.querySelector('#buttonClear');
-buttonClear.addEventListener('click', createGrids);
+buttonClear.onclick = function () {
+    createGrids(currentGridValue);
+};
